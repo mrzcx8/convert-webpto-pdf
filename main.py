f43740b7db1322af2fe75c8c2c3745625dc8f99c
Mrzcx8 <121825477+mrzcx8@users.noreply.github.com>
2025-05-13 21:02:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import traceback
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20MB max upload size
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 20MB max upload size
 
 UPLOAD_FOLDER = 'static/uploads'
 OUTPUT_FOLDER = 'output'
